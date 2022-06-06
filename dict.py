46429da8aab734015833ff5a9da5ea70e41defd3
Andrew Kleonskiy <co95310a@mail.ru>
2022-06-06 22:49:43 +0300

    dictionary

--- dict.py
+++ dict.py
@@ -1,2 +1,4 @@
 D = {'spam':2,'ham':1,'eggs':3}
 print(D['spam'])
+list(D.items())
+list(D.values())
---
+++ dictionary.py
@@ -0,0 +1,4 @@
+D = {'spam':2,'ham':1,'eggs':3}
+print(list(D.items()))
+print(list(D.values()))
+print(D.get('spams',22))
